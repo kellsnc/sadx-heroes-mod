eaa8906776e2ea861526da13479c8ff373e10b49
Kell <kellspam@outlook.fr>
2019-02-22 01:21:27 +0100

    Update Ocean Palace objects

--- sadx-heroes-mod/ocean-palace-objects.cpp
+++ sadx-heroes-mod/ocean-palace-objects.cpp
@@ -4,6 +4,179 @@
 
 #include "ocean-palace-objects.h"
 
+ModelInfo * OP_WATERFS;
+ModelInfo * OP_FLOWERS;
+ModelInfo * OP_TURFINS;
+ModelInfo * OP_BOULDER;
+ModelInfo * OP_POLFLAG;
+
+static int finsstate = 0;
+
+static uint8_t rocktrigger = 0;
+static uint8_t rockstate = 0;
+static float rocktimer = 0;
+static uint8_t rockstate2 = 0;
+static float rocktimer2 = 0;
+static uint8_t rockstate3 = 0;
+static float rocktimer3 = 0;
+
+NJS_VECTOR OceanPalaceTriggers[]{
+	{ -8200, 1401, -40045 },
+	{ -8350, 1301, -43101 }
+};
+
+#pragma region Fins
+void OPFins_Display(ObjectMaster *a1) {
+	if (!DroppedFrames) {
+		for (int i = 0; i < LengthOfArray(OceanPalace_EventObjects); ++i) {
+			if (OceanPalace_EventObjects[i].Model > 1 && CheckModelDisplay(OceanPalace_EventObjects[i])) {
+				SOI_LIST2 item = OceanPalace_EventObjects[i];
+
+				njSetTexture((NJS_TEXLIST*)CurrentLevelTexlist);
+				njPushMatrix(0);
+				njTranslate(nullptr, item.Position.x, item.Position.y, item.Position.z);
+				njRotateXYZ(nullptr, item.Rotation[0], item.Rotation[1], item.Rotation[2]);
+				njScale(nullptr, item.Scale.x, item.Scale.y, item.Scale.z);
+				DrawQueueDepthBias = item.Bias;
+
+				switch (item.Model) {
+				case 2: njDrawModel_SADX(OP_TURFINS->getmodel()->basicdxmodel); break;
+				case 3: njDrawModel_SADX(OP_TURFINS->getmodel()->child->basicdxmodel); break;
+				case 4: njDrawModel_SADX(OP_TURFINS->getmodel()->child->child->basicdxmodel); break;
+				case 5: njDrawModel_SADX(OP_TURFINS->getmodel()->child->child->child->basicdxmodel); break;
+				}
+
+				DrawQueueDepthBias = 0;
+				njPopMatrix(1u);
+			}
+		}
+	}
+}
+
+void OPFins_Main(ObjectMaster *a1) {
+	if (CurrentChunk == 5) {
+		finsstate++;
+		if (finsstate > 300) finsstate = 0;
+		for (Int i = 9; i < 41; ++i) {
+			if (finsstate <= 100) {
+				OceanPalace_EventObjects[i].Rotation[1] -= 100;
+			}
+			if (finsstate > 100) {
+				OceanPalace_EventObjects[i].Rotation[1] += 50.5f;
+			}
+		}
+		for (Int i = 41; i < 73; ++i) {
+			if (finsstate <= 100) {
+				OceanPalace_EventObjects[i].Rotation[1] += 100;
+			}
+			if (finsstate > 100) {
+				OceanPalace_EventObjects[i].Rotation[1] -= 50.5f;
+			}
+		}
+		for (Int i = 9; i < 73; ++i) {
+			if (finsstate <= 100) {
+				OceanPalace_EventObjects[i].Rotation[0] += 10;
+			}
+			if (finsstate > 100) {
+				OceanPalace_EventObjects[i].Rotation[0] -= 5;
+			}
+		}
+
+		OPFins_Display(a1);
+	}
+}
+
+void OPFins(ObjectMaster *a1) {
+	a1->DisplaySub = OPFins_Display;
+	a1->MainSub = OPFins_Main;
+}
+#pragma endregion
+
+#pragma region Flowers
+void OPFlowers_Display(ObjectMaster *a1) {
+	if (!DroppedFrames) {
+		for (int i = 0; i < LengthOfArray(OceanPalace_Flowers); ++i) {
+			if (CheckModelDisplay(OceanPalace_Flowers[i])) {
+				SOI_LIST2 item = OceanPalace_Flowers[i];
+
+				njSetTexture((NJS_TEXLIST*)CurrentLevelTexlist);
+				njPushMatrix(0);
+				njTranslate(nullptr, item.Position.x, item.Position.y, item.Position.z);
+				njRotateXYZ(nullptr, item.Rotation[0], item.Rotation[1], item.Rotation[2]);
+				njScale(nullptr, item.Scale.x, item.Scale.y, item.Scale.z);
+				DrawQueueDepthBias = item.Bias;
+
+				switch (item.Model) {
+				case 0: njDrawModel_SADX(OP_FLOWERS->getmodel()->basicdxmodel); break;
+				case 1: njDrawModel_SADX(OP_FLOWERS->getmodel()->child->basicdxmodel); break;
+				case 2: njDrawModel_SADX(OP_FLOWERS->getmodel()->child->child->basicdxmodel); break;
+				case 3: njDrawModel_SADX(OP_FLOWERS->getmodel()->child->child->child->basicdxmodel); break;
+				}
+
+				DrawQueueDepthBias = 0;
+				njPopMatrix(1u);
+			}
+		}
+	}
+}
+
+void OPFlowers(ObjectMaster *a1) {
+	a1->DisplaySub = OPFlowers_Display;
+	a1->MainSub = OPFlowers_Display;
+}
+#pragma endregion
+
+#pragma region Waterfalls
+void OPWaterfalls_Display(ObjectMaster *a1) {
+	if (!DroppedFrames) {
+		if (CurrentAct == 0) {
+			for (int i = 0; i < LengthOfArray(OceanPalace_Waterfalls); ++i) {
+				if (CheckModelDisplay(OceanPalace_Waterfalls[i])) {
+					SOI_LIST2 item = OceanPalace_Waterfalls[i];
+
+					njSetTexture((NJS_TEXLIST*)CurrentLevelTexlist);
+					njPushMatrix(0);
+					njTranslate(nullptr, item.Position.x, item.Position.y, item.Position.z);
+					njRotateXYZ(nullptr, item.Rotation[0], item.Rotation[1], item.Rotation[2]);
+					njScale(nullptr, item.Scale.x, item.Scale.y, item.Scale.z);
+					DrawQueueDepthBias = item.Bias;
+
+					switch (item.Model) {
+					case 0: njDrawModel_SADX(OP_WATERFS->getmodel()->basicdxmodel); break;
+					case 1: njDrawModel_SADX(OP_WATERFS->getmodel()->child->basicdxmodel); break;
+					case 2: njDrawModel_SADX(OP_WATERFS->getmodel()->child->child->basicdxmodel); break;
+					case 3: njDrawModel_SADX(OP_WATERFS->getmodel()->child->child->child->basicdxmodel); break;
+					case 4: njDrawModel_SADX(OP_WATERFS->getmodel()->child->child->child->child->basicdxmodel); break;
+					case 5: njDrawModel_SADX(OP_WATERFS->getmodel()->child->child->child->child->child->basicdxmodel); break;
+					case 6: njDrawModel_SADX(OP_WATERFS->getmodel()->child->child->child->child->child->child->basicdxmodel); break;
+					case 7: njDrawModel_SADX(OP_WATERFS->getmodel()->child->child->child->child->child->child->child->basicdxmodel); break;
+					case 8: njDrawModel_SADX(OP_WATERFS->getmodel()->child->child->child->child->child->child->child->child->basicdxmodel); break;
+					case 9: njDrawModel_SADX(OP_WATERFS->getmodel()->child->child->child->child->child->child->child->child->child->basicdxmodel); break;
+					}
+
+					DrawQueueDepthBias = 0;
+					njPopMatrix(1u);
+				}
+			}
+		}
+		else {
+			njSetTexture((NJS_TEXLIST*)CurrentLevelTexlist);
+			njPushMatrix(0);
+			DrawQueueDepthBias = -6000.f;
+			njTranslate(nullptr, 0, 3000, 2900);
+			njDrawModel_SADX(OP_WATERFS->getmodel()->child->child->child->child->child->child->child->child->child->child->child->child->basicdxmodel);
+			DrawQueueDepthBias = 0;
+			njPopMatrix(1u);
+		}
+	}
+}
+
+void OPWaterfalls(ObjectMaster *a1) {
+	a1->DisplaySub = OPWaterfalls_Display;
+	a1->MainSub = OPWaterfalls_Display;
+}
+#pragma endregion
+
 #pragma region Poles
 void OPPOLE_Display(ObjectMaster *a1) {
 	if (!MissedFrames && IsPlayerInsideSphere(&a1->Data1->Position, 2000.0f)) {
@@ -13,14 +186,9 @@ void OPPOLE_Display(ObjectMaster *a1) {
 		njRotateXYZ(nullptr, a1->Data1->Rotation.x, a1->Data1->Rotation.y, a1->Data1->Rotation.z);
 		njScale(nullptr, 1, 1, 1);
 		DrawQueueDepthBias = -6000.0f;
-		if (a1->Data1->Scale.x == 1) {
-			njDrawModel_SADX(OP_PoleR.basicdxmodel);
-			njDrawModel_SADX(&OP_PoleRWater);
-		}
-		else {
-			njDrawModel_SADX(OP_PoleL.basicdxmodel);
-			njDrawModel_SADX(&OP_PoleLWater);
-		}
+		njDrawModel_SADX(a1->Data1->Object->basicdxmodel);
+		if (a1->Data1->Scale.x == 1) njDrawModel_SADX(OP_WATERFS->getmodel()->child->child->child->child->child->child->child->child->child->child->child->basicdxmodel);
+		else njDrawModel_SADX(OP_WATERFS->getmodel()->child->child->child->child->child->child->child->child->child->child->basicdxmodel);
 		DrawQueueDepthBias = 0;
 		njPopMatrix(1u);
 	}
@@ -37,13 +205,136 @@ void OPPOLE_Main(ObjectMaster *a1) {
 
 void __cdecl OPPOLE(ObjectMaster *a1)
 {
-	if (a1->Data1->Scale.x == 1) a1->Data1->Object = &OP_PoleR;
-	else a1->Data1->Object = &OP_PoleL;
+	if (a1->Data1->Scale.x == 1) a1->Data1->Object = OP_POLFLAG->getmodel()->child;
+	else a1->Data1->Object = OP_POLFLAG->getmodel();
 	AddToCollision(a1, 0);
 
 	a1->MainSub = &OPPOLE_Main;
 	a1->DisplaySub = &OPPOLE_Display;
 	a1->DeleteSub = &deleteSub_Global;
+}
+#pragma endregion
+
+#pragma region Boulders
+void OP_AnimFix() {
+	auto entity = EntityData1Ptrs[0];
+	CharObj2 *co2 = GetCharObj2(0);
+	if (!SuperSonicFlag) {
+		entity->Action = 2;
+		co2->AnimationThing.Index = 13;
+	}
+	else {
+		entity->Action = 76;
+		co2->AnimationThing.Index = 137;
+	}
+}
+
+void OPBoulders_Display(ObjectMaster *a1) {
+	if (!DroppedFrames) {
+		for (int i = 2; i < 5; ++i) {
+			if (i > 2 && CurrentChunk != 7) break;
+
+			SOI_LIST2 item = OceanPalace_EventObjects[i];
+
+			njSetTexture((NJS_TEXLIST*)CurrentLevelTexlist);
+			njPushMatrix(0);
+			njTranslate(nullptr, item.Position.x, item.Position.y, item.Position.z);
+			njRotateXYZ(nullptr, item.Rotation[0], item.Rotation[1], item.Rotation[2]);
+			njScale(nullptr, item.Scale.x, item.Scale.y, item.Scale.z);
+			DrawQueueDepthBias = item.Bias;
+
+			njDrawModel_SADX(OP_BOULDER->getmodel()->basicdxmodel);
+
+			DrawQueueDepthBias = 0;
+			njPopMatrix(1u);
+		}
+	}
+}
+
+void OPBoulders_Delete(ObjectMaster *a1) {
+	rocktrigger = 0;
+	rockstate = 0;
+	rocktimer = 0;
+	rockstate2 = 0;
+	rocktimer2 = 0;
+	rockstate3 = 0;
+	rocktimer3 = 0;
+
+	OceanPalace_EventObjects[2].Position = { -8200.3408f, 2030, -39259.3f };
+	OceanPalace_EventObjects[3].Position = { -8274.9741f, 1363, -44980.36f };
+	OceanPalace_EventObjects[4].Position = { -6785.288f, 963, -46514.48f };
+
+	OceanPalace_EventObjects[2].Rotation[1] = 32768;
+	OceanPalace_EventObjects[3].Rotation[1] = 24735;
+	OceanPalace_EventObjects[4].Rotation[1] = 32768;
+}
+
+void OPBoulders_Main(ObjectMaster *a1) {
+	//todo: rewrite this shit
+
+	if (CurrentChunk > 5) {
+		if (IsPlayerInsideSphere(&OceanPalaceTriggers[1], 45.0f) == 1) OP_AnimFix();
+
+		if (IsPlayerInsideSphere(&OceanPalaceTriggers[0], 45.0f) == 1) {
+			rocktrigger = 1;
+			OP_AnimFix();
+			FreeCam = 0;
+		}
+
+		if (OceanPalace_EventObjects[2].Position.z < -44954) rocktrigger = 2;
+		if (OceanPalace_EventObjects[2].Position.z < -46553) rocktrigger = 3;
+
+		if (rocktrigger > 0) {
+			if (IsPlayerInsideSphere(&OceanPalace_EventObjects[2].Position, 120.0f) == 1 || IsPlayerInsideSphere(&OceanPalace_EventObjects[3].Position, 120.0f) == 1 || IsPlayerInsideSphere(&OceanPalace_EventObjects[4].Position, 120.0f) == 1) GameState = 7;
+
+			OceanPalace_EventObjects[2].Rotation[0] = OceanPalace_EventObjects[2].Rotation[0] + 1500;
+			if (rocktimer > 1) { rocktimer = 0; rockstate++; }
+
+			if (rockstate < LengthOfArray(OP_ROCK1) - 1) {
+				rocktimer = rocktimer + (OP_ROCKS[0].totaldist / OP_ROCK1[rockstate].Distance) / OP_ROCKS[0].totaldist * 8;
+				if (OP_ROCK1[rockstate].Rotation[1] != 0) {
+					OceanPalace_EventObjects[2].Rotation[1] = OP_ROCK1[rockstate].Rotation[1];
+				}
+				OceanPalace_EventObjects[2].Position.x = (OP_ROCK1[rockstate + 1].Position.x - OP_ROCK1[rockstate].Position.x) * rocktimer + OP_ROCK1[rockstate].Position.x;
+				OceanPalace_EventObjects[2].Position.y = (OP_ROCK1[rockstate + 1].Position.y - OP_ROCK1[rockstate].Position.y) * rocktimer + OP_ROCK1[rockstate].Position.y;
+				OceanPalace_EventObjects[2].Position.z = (OP_ROCK1[rockstate + 1].Position.z - OP_ROCK1[rockstate].Position.z) * rocktimer + OP_ROCK1[rockstate].Position.z;
+			}
+			if (rocktrigger > 1) {
+				OceanPalace_EventObjects[3].Rotation[0] = OceanPalace_EventObjects[3].Rotation[0] + 1500;
+				if (rocktimer2 > 1) { rocktimer2 = 0; rockstate2++; }
+				if (rockstate2 < LengthOfArray(OP_ROCK2) - 1) {
+					rocktimer2 = rocktimer2 + (OP_ROCKS[0].totaldist / OP_ROCK2[rockstate2].Distance) / OP_ROCKS[0].totaldist * 8;
+					if (OP_ROCK2[rockstate2].Rotation[1] != 0) {
+						OceanPalace_EventObjects[3].Rotation[1] = OP_ROCK2[rockstate2].Rotation[1];
+					}
+					OceanPalace_EventObjects[3].Position.x = (OP_ROCK2[rockstate2 + 1].Position.x - OP_ROCK2[rockstate2].Position.x) * rocktimer2 + OP_ROCK2[rockstate2].Position.x;
+					OceanPalace_EventObjects[3].Position.y = (OP_ROCK2[rockstate2 + 1].Position.y - OP_ROCK2[rockstate2].Position.y) * rocktimer2 + OP_ROCK2[rockstate2].Position.y;
+					OceanPalace_EventObjects[3].Position.z = (OP_ROCK2[rockstate2 + 1].Position.z - OP_ROCK2[rockstate2].Position.z) * rocktimer2 + OP_ROCK2[rockstate2].Position.z;
+				}
+				if (rocktrigger > 2) {
+					OceanPalace_EventObjects[4].Rotation[0] = OceanPalace_EventObjects[4].Rotation[0] + 1500;
+					if (rocktimer3 > 1) { rocktimer3 = 0; rockstate3++; }
+					if (rockstate3 < LengthOfArray(OP_ROCK3) - 1) {
+						rocktimer3 = rocktimer3 + (OP_ROCKS[0].totaldist / OP_ROCK3[rockstate3].Distance) / OP_ROCKS[0].totaldist * 8;
+						if (OP_ROCK3[rockstate3].Rotation[1] != 0) {
+							OceanPalace_EventObjects[4].Rotation[1] = OP_ROCK3[rockstate3].Rotation[1];
+						}
+						OceanPalace_EventObjects[4].Position.x = (OP_ROCK3[rockstate3 + 1].Position.x - OP_ROCK3[rockstate3].Position.x) * rocktimer3 + OP_ROCK3[rockstate3].Position.x;
+						OceanPalace_EventObjects[4].Position.y = (OP_ROCK3[rockstate3 + 1].Position.y - OP_ROCK3[rockstate3].Position.y) * rocktimer3 + OP_ROCK3[rockstate3].Position.y;
+						OceanPalace_EventObjects[4].Position.z = (OP_ROCK3[rockstate3 + 1].Position.z - OP_ROCK3[rockstate3].Position.z) * rocktimer3 + OP_ROCK3[rockstate3].Position.z;
+					}
+				}
+			}
+		}
+
+		OPBoulders_Display(a1);
+	}
+}
+
+void OPBoulders(ObjectMaster *a1) {
+	a1->DisplaySub = OPBoulders_Display;
+	a1->DeleteSub = OPBoulders_Delete;
+	a1->MainSub = OPBoulders_Main;
 }
 #pragma endregion
 
@@ -162,8 +453,4 @@ void OceanPalaceObjects_Init(const char *path) {
 	WriteData((PVMEntry**)0x90EB70, OceanPalaceTextures);
 	WriteData((ObjectList**)0x974B38, &OceanPalaceObjectList);
 	WriteData((ObjectList**)0x974B3C, &OceanPalaceObjectList);
-}
-
-void OceanPalaceObjects_OnFrame(EntityData1 * entity) {
-	AnimateUV(OceanPalace_UVSHIFT, LengthOfArray(OceanPalace_UVSHIFT));
 }
--- sadx-heroes-mod/ocean-palace.cpp
+++ sadx-heroes-mod/ocean-palace.cpp
@@ -6,132 +6,44 @@
 #include "ocean-palace.h"
 
 void OceanPalaceObjects_Init(const char *path);
-void OceanPalaceObjects_OnFrame(EntityData1 * entity);
 void SHSuns_Init(ObjectMaster * a1);
 
-extern SOI_LIST ocean_palace_objects_common[];
-extern SOI_LISTS ocean_palace_objects[];
-extern SOI_LISTS road_rock_objects[];
+void OPFins(ObjectMaster *a1);
+void OPFlowers(ObjectMaster *a1);
+void OPWaterfalls(ObjectMaster *a1);
+void OPBoulders(ObjectMaster *a1);
+
 extern DeathZone SeasideHillDeathZones[];
 
-static int finsstate = 0;
-static uint8_t rocktrigger = 0;
-static uint8_t rockstate = 0;
-static float rocktimer = 0;
-static uint8_t rockstate2 = 0;
-static float rocktimer2 = 0;
-static uint8_t rockstate3 = 0;
-static float rocktimer3 = 0;
+#pragma region Objects Data
+SH_UVSHIFT OceanPalace_UVShift[]{
+	{ nullptr,0,{ 0, -10 } },
+	{ nullptr,0,{ 0, -10 } },
+	{ nullptr,0,{ 0, -10 } },
+	{ nullptr,0,{ 0, -10 } },
+	{ nullptr,0,{ 0, -10 } },
+	{ nullptr,0,{ 0, -10 } },
+	{ nullptr,0,{ 0, -10 } },
+	{ nullptr,0,{ 0, -10 } },
+	{ nullptr,0,{ 0, -10 } },
+	{ nullptr,0,{ 0, -10 } },
+	{ nullptr,0,{ 0, -10 } },
+	{ nullptr,0,{ 0, -10 } },
+	{ nullptr,0,{ 0, -10 } },
+	{ nullptr,0,{ 0, -10 } },
+};
 
-//Sometimes sonic skips the running animation
-void OP_AnimFix() {
-	auto entity = EntityData1Ptrs[0];
-	CharObj2 *co2 = GetCharObj2(0);
-	if (!SuperSonicFlag) {
-		entity->Action = 2;
-		co2->AnimationThing.Index = 13;
-	}
-	else {
-		entity->Action = 76;
-		co2->AnimationThing.Index = 137;
-	}
-}
-
-//A huge mess that was supposed to be temporary but then got lazy to rewrite
-//Seriously though it's horrible
-void GiantRockHandler() {
-	if (IsPlayerInsideSphere(&OceanPalaceTriggers[1], 45.0f) == 1) OP_AnimFix();
-
-	if (IsPlayerInsideSphere(&OceanPalaceTriggers[0], 45.0f) == 1) {
-		rocktrigger = 1;
-		OP_AnimFix();
-		FreeCam = 0;
-	}
-
-	if (ocean_palace_objects_common[2].Position.z < -44954) rocktrigger = 2;
-	if (ocean_palace_objects_common[2].Position.z < -46553) rocktrigger = 3;
-
-	if (rocktrigger > 0) {
-		if (IsPlayerInsideSphere(&ocean_palace_objects_common[2].Position, 120.0f) == 1 || IsPlayerInsideSphere(&ocean_palace_objects_common[3].Position, 120.0f) == 1 || IsPlayerInsideSphere(&ocean_palace_objects_common[4].Position, 120.0f) == 1) GameState = 7;
-
-		ocean_palace_objects_common[2].Rotation[0] = ocean_palace_objects_common[2].Rotation[0] + 1500;
-		if (rocktimer > 1) { rocktimer = 0; rockstate++; }
-
-		if (rockstate < LengthOfArray(OP_ROCK1) - 1) {
-			rocktimer = rocktimer + (OP_ROCKS[0].totaldist / OP_ROCK1[rockstate].Distance) / OP_ROCKS[0].totaldist * 8;
-			if (OP_ROCK1[rockstate].Rotation[1] != 0) {
-				ocean_palace_objects_common[2].Rotation[1] = OP_ROCK1[rockstate].Rotation[1];
-			}
-			ocean_palace_objects_common[2].Position.x = (OP_ROCK1[rockstate + 1].Position.x - OP_ROCK1[rockstate].Position.x) * rocktimer + OP_ROCK1[rockstate].Position.x;
-			ocean_palace_objects_common[2].Position.y = (OP_ROCK1[rockstate + 1].Position.y - OP_ROCK1[rockstate].Position.y) * rocktimer + OP_ROCK1[rockstate].Position.y;
-			ocean_palace_objects_common[2].Position.z = (OP_ROCK1[rockstate + 1].Position.z - OP_ROCK1[rockstate].Position.z) * rocktimer + OP_ROCK1[rockstate].Position.z;
-		}
-		if (rocktrigger > 1) {
-			ocean_palace_objects_common[3].Rotation[0] = ocean_palace_objects_common[3].Rotation[0] + 1500;
-			if (rocktimer2 > 1) { rocktimer2 = 0; rockstate2++; }
-			if (rockstate2 < LengthOfArray(OP_ROCK2) - 1) {
-				rocktimer2 = rocktimer2 + (OP_ROCKS[0].totaldist / OP_ROCK2[rockstate2].Distance) / OP_ROCKS[0].totaldist * 8;
-				if (OP_ROCK2[rockstate2].Rotation[1] != 0) {
-					ocean_palace_objects_common[3].Rotation[1] = OP_ROCK2[rockstate2].Rotation[1];
-				}
-				ocean_palace_objects_common[3].Position.x = (OP_ROCK2[rockstate2 + 1].Position.x - OP_ROCK2[rockstate2].Position.x) * rocktimer2 + OP_ROCK2[rockstate2].Position.x;
-				ocean_palace_objects_common[3].Position.y = (OP_ROCK2[rockstate2 + 1].Position.y - OP_ROCK2[rockstate2].Position.y) * rocktimer2 + OP_ROCK2[rockstate2].Position.y;
-				ocean_palace_objects_common[3].Position.z = (OP_ROCK2[rockstate2 + 1].Position.z - OP_ROCK2[rockstate2].Position.z) * rocktimer2 + OP_ROCK2[rockstate2].Position.z;
-			}
-			if (rocktrigger > 2) {
-				ocean_palace_objects_common[4].Rotation[0] = ocean_palace_objects_common[4].Rotation[0] + 1500;
-				if (rocktimer3 > 1) { rocktimer3 = 0; rockstate3++; }
-				if (rockstate3 < LengthOfArray(OP_ROCK3) - 1) {
-					rocktimer3 = rocktimer3 + (OP_ROCKS[0].totaldist / OP_ROCK3[rockstate3].Distance) / OP_ROCKS[0].totaldist * 8;
-					if (OP_ROCK3[rockstate3].Rotation[1] != 0) {
-						ocean_palace_objects_common[4].Rotation[1] = OP_ROCK3[rockstate3].Rotation[1];
-					}
-					ocean_palace_objects_common[4].Position.x = (OP_ROCK3[rockstate3 + 1].Position.x - OP_ROCK3[rockstate3].Position.x) * rocktimer3 + OP_ROCK3[rockstate3].Position.x;
-					ocean_palace_objects_common[4].Position.y = (OP_ROCK3[rockstate3 + 1].Position.y - OP_ROCK3[rockstate3].Position.y) * rocktimer3 + OP_ROCK3[rockstate3].Position.y;
-					ocean_palace_objects_common[4].Position.z = (OP_ROCK3[rockstate3 + 1].Position.z - OP_ROCK3[rockstate3].Position.z) * rocktimer3 + OP_ROCK3[rockstate3].Position.z;
-				}
-			}
-		}
-	}
-}
-
-//Got lazy to make turtle fins an object so hardcode everything
-void FinsHandler() {
-	if (CurrentChunk == 5) {
-		finsstate++;
-		if (finsstate > 300) finsstate = 0;
-		for (Int i = 9; i < 41; ++i) {
-			if (finsstate <= 100) {
-				ocean_palace_objects_common[i].Rotation[1] -= 100;
-			}
-			if (finsstate > 100) {
-				ocean_palace_objects_common[i].Rotation[1] += 50.5f;
-			}
-		}
-		for (Int i = 41; i < 73; ++i) {
-			if (finsstate <= 100) {
-				ocean_palace_objects_common[i].Rotation[1] += 100;
-			}
-			if (finsstate > 100) {
-				ocean_palace_objects_common[i].Rotation[1] -= 50.5f;
-			}
-		}
-		for (Int i = 9; i < 73; ++i) {
-			if (finsstate <= 100) {
-				ocean_palace_objects_common[i].Rotation[0] += 10;
-			}
-			if (finsstate > 100) {
-				ocean_palace_objects_common[i].Rotation[0] -= 5;
-			}
-		}
-	}
-}
+extern ModelInfo * OP_WATERFS;
+extern ModelInfo * OP_FLOWERS;
+extern ModelInfo * OP_TURFINS;
+extern ModelInfo * OP_BOULDER;
+extern ModelInfo * OP_POLFLAG;
+#pragma endregion
 
 void OceanPalace_OnFrame(EntityData1 * entity, CharObj2 * co2) {
 	if (CurrentLandTable) {
 		if (anim % 4 == 0) {
 			if (entity != nullptr) {
-
 				CurrentLandTable->Col[0].Model->pos[0] = entity->Position.x;
 				CurrentLandTable->Col[0].Model->pos[2] = entity->Position.z;
 				CurrentLandTable->Col[1].Model->pos[0] = entity->Position.x;
@@ -143,28 +55,53 @@ void OceanPalace_OnFrame(EntityData1 * entity, CharObj2 * co2) {
 				}
 			}
 		}
-
-		FinsHandler();
-		GiantRockHandler();
 	}
 }
 
+void OceanPalace_InitObjects() {
+	OP_FLOWERS = LoadMDL("OP_FLOWERS");
+	OP_TURFINS = LoadMDL("OP_TURFINS");
+	OP_WATERFS = LoadMDL("OP_WATERFS");
+	OP_BOULDER = LoadMDL("OP_BOULDER");
+	OP_POLFLAG = LoadMDL("OP_POLFLAG");
+
+	OceanPalace_UVShift[0].List = OP_WATERFS->getmodel()->basicdxmodel->meshsets[0].vertuv;
+	OceanPalace_UVShift[1].List = OP_WATERFS->getmodel()->child->basicdxmodel->meshsets[0].vertuv;
+	OceanPalace_UVShift[2].List = OP_WATERFS->getmodel()->child->child->basicdxmodel->meshsets[0].vertuv;
+	OceanPalace_UVShift[3].List = OP_WATERFS->getmodel()->child->child->child->basicdxmodel->meshsets[0].vertuv;
+	OceanPalace_UVShift[4].List = OP_WATERFS->getmodel()->child->child->child->child->basicdxmodel->meshsets[0].vertuv;
+	OceanPalace_UVShift[5].List = OP_WATERFS->getmodel()->child->child->child->child->child->basicdxmodel->meshsets[0].vertuv;
+	OceanPalace_UVShift[6].List = OP_WATERFS->getmodel()->child->child->child->child->child->child->basicdxmodel->meshsets[0].vertuv;
+	OceanPalace_UVShift[7].List = OP_WATERFS->getmodel()->child->child->child->child->child->child->child->basicdxmodel->meshsets[0].vertuv;
+	OceanPalace_UVShift[8].List = OP_WATERFS->getmodel()->child->child->child->child->child->child->child->child->basicdxmodel->meshsets[0].vertuv;
+	OceanPalace_UVShift[9].List = OP_WATERFS->getmodel()->child->child->child->child->child->child->child->child->child->basicdxmodel->meshsets[0].vertuv;
+	OceanPalace_UVShift[10].List = OP_WATERFS->getmodel()->child->child->child->child->child->child->child->child->child->child->basicdxmodel->meshsets[0].vertuv;
+	OceanPalace_UVShift[11].List = OP_WATERFS->getmodel()->child->child->child->child->child->child->child->child->child->child->child->basicdxmodel->meshsets[0].vertuv;
+	OceanPalace_UVShift[12].List = OP_WATERFS->getmodel()->child->child->child->child->child->child->child->child->child->child->child->child->basicdxmodel->meshsets[0].vertuv;
+	OceanPalace_UVShift[12].List = OP_WATERFS->getmodel()->child->child->child->child->child->child->child->child->child->child->child->child->basicdxmodel->meshsets[1].vertuv;
+
+	OceanPalace_UVShift[0].Size = OP_WATERFS->getmodel()->basicdxmodel->meshsets[0].nbMesh * 3;
+	OceanPalace_UVShift[1].Size = OP_WATERFS->getmodel()->child->basicdxmodel->meshsets[0].nbMesh * 3;
+	OceanPalace_UVShift[2].Size = OP_WATERFS->getmodel()->child->child->basicdxmodel->meshsets[0].nbMesh * 3;
+	OceanPalace_UVShift[3].Size = OP_WATERFS->getmodel()->child->child->child->basicdxmodel->meshsets[0].nbMesh * 3;
+	OceanPalace_UVShift[4].Size = OP_WATERFS->getmodel()->child->child->child->child->basicdxmodel->meshsets[0].nbMesh * 3;
+	OceanPalace_UVShift[5].Size = OP_WATERFS->getmodel()->child->child->child->child->child->basicdxmodel->meshsets[0].nbMesh * 3;
+	OceanPalace_UVShift[6].Size = OP_WATERFS->getmodel()->child->child->child->child->child->child->basicdxmodel->meshsets[0].nbMesh * 3;
+	OceanPalace_UVShift[7].Size = OP_WATERFS->getmodel()->child->child->child->child->child->child->child->basicdxmodel->meshsets[0].nbMesh * 3;
+	OceanPalace_UVShift[8].Size = OP_WATERFS->getmodel()->child->child->child->child->child->child->child->child->basicdxmodel->meshsets[0].nbMesh * 3;
+	OceanPalace_UVShift[9].Size = OP_WATERFS->getmodel()->child->child->child->child->child->child->child->child->child->basicdxmodel->meshsets[0].nbMesh * 3;
+	OceanPalace_UVShift[10].Size = OP_WATERFS->getmodel()->child->child->child->child->child->child->child->child->child->child->basicdxmodel->meshsets[0].nbMesh * 3;
+	OceanPalace_UVShift[11].Size = OP_WATERFS->getmodel()->child->child->child->child->child->child->child->child->child->child->child->basicdxmodel->meshsets[0].nbMesh * 3;
+	OceanPalace_UVShift[12].Size = OP_WATERFS->getmodel()->child->child->child->child->child->child->child->child->child->child->child->child->basicdxmodel->meshsets[0].nbMesh * 3;
+	OceanPalace_UVShift[12].Size = OP_WATERFS->getmodel()->child->child->child->child->child->child->child->child->child->child->child->child->basicdxmodel->meshsets[1].nbMesh * 3;
+}
+
 void OceanPalace_delete(ObjectMaster * a1) {
-	rocktrigger = 0;
-	rockstate = 0;
-	rocktimer = 0;
-	rockstate2 = 0;
-	rocktimer2 = 0;
-	rockstate3 = 0;
-	rocktimer3 = 0;
-
-	ocean_palace_objects_common[2].Position = { -8200.3408f, 2030, -39259.3f };
-	ocean_palace_objects_common[3].Position = { -8274.9741f, 1363, -44980.36f };
-	ocean_palace_objects_common[4].Position = { -6785.288f, 963, -46514.48f };
-
-	ocean_palace_objects_common[2].Rotation[1] = 32768;
-	ocean_palace_objects_common[3].Rotation[1] = 24735;
-	ocean_palace_objects_common[4].Rotation[1] = 32768;
+	FreeMDL(OP_TURFINS);
+	FreeMDL(OP_POLFLAG);
+	FreeMDL(OP_FLOWERS);
+	FreeMDL(OP_BOULDER);
+	FreeMDL(OP_WATERFS);
 
 	LevelHandler_Delete(a1);
 }
@@ -177,6 +114,10 @@ void OceanPalaceHandler(ObjectMaster * a1) {
 		a1->Data1->Action = 1;
 		a1->DeleteSub = OceanPalace_delete;
 
+		OceanPalace_InitObjects();
+
+		LoadObject(LoadObj_Data1, 3, OPWaterfalls);
+
 		if (CurrentAct == 0) {
 			//Ocean Palace
 			PlaySound(44, 0, 0, 0);
@@ -188,9 +129,10 @@ void OceanPalaceHandler(ObjectMaster * a1) {
 			CurrentLandAddress = (LandTable**)0x97DA48;
 			matlist_waterfall[0].attr_texId = 66;
 
-			ObjectMaster * modelhandler = LoadObject(LoadObj_Data1, 3, ModelHandler_Init);
-			modelhandler->Data1->LoopData = (Loop*)&ocean_palace_objects;
-			LoadObject(LoadObj_Data1, 3, SHSuns_Init); //load the sun
+			LoadObject(LoadObj_Data1, 3, OPFlowers);
+			LoadObject(LoadObj_Data1, 3, OPFins);
+			LoadObject(LoadObj_Data1, 3, OPBoulders);
+			LoadObject(LoadObj_Data1, 3, SHSuns_Init)->Data1->Position = { 74, 11106, 425 }; //load the sun
 
 			if (entity->Position.z > -10637) LoadLevelFile("OP", 01);
 		}
@@ -204,22 +146,20 @@ void OceanPalaceHandler(ObjectMaster * a1) {
 			CurrentLandAddress = (LandTable**)0x97DA4C;
 			matlist_waterfall[0].attr_texId = 45;
 
-			ObjectMaster * modelhandler = LoadObject(LoadObj_Data1, 3, ModelHandler_Init);
-			modelhandler->Data1->LoopData = (Loop*)&road_rock_objects;
 			LoadLevelFile("RR", 01);
 		}
 	}
 	else {
+		AnimateUV(OceanPalace_UVShift, LengthOfArray(OceanPalace_UVShift));
+
 		switch (CurrentAct) {
 		case 0:
 			ChunkHandler("OP", OceanPalaceChunks, LengthOfArray(OceanPalaceChunks), entity->Position);
 			AnimateTextures(OceanPalaceAnimTexs, LengthOfArray(OceanPalaceAnimTexs));
 			OceanPalace_OnFrame(entity, co2);
-			OceanPalaceObjects_OnFrame(entity);
 			break;
 		case 1:
 			AnimateTextures(RoadRockAnimTexs, LengthOfArray(RoadRockAnimTexs));
-			OceanPalaceObjects_OnFrame(entity);
 			break;
 		}
 	}
