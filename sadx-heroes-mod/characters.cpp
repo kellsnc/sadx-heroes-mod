5163da9399db616e2dc63a47225188229cefdf90
Kell <kellspam@outlook.fr>
2019-11-11 02:57:14 +0100

    Animation system, fixed crash

--- sadx-heroes-mod/characters.cpp
+++ sadx-heroes-mod/characters.cpp
@@ -6,7 +6,9 @@ AnimationFile* CreamAnms[63];
 AnimData CreamAnimData[63];
 bool CreamEnabled = false;
 bool CreamLoaded;
-bool Creams[8];
+ObjectMaster* Creams[8];
+
+bool CharTexsLoaded[1];
 
 enum CharactersHeroes
 {
@@ -18,6 +20,7 @@ NJS_TEXLIST CREAM_TEXLIST = { arrayptrandlength(CREAM_TEXNAMES) };
 
 #pragma region Chunk stuff
 FastcallFunctionPointer(void, DrawChunkModel_, (Sint32* a1, Sint16* a2), 0x7917F0);
+
 D3DMATRIX WorldMatrixBackup;
 
 void DrawChunkModel(NJS_CNK_MODEL* model)
@@ -55,13 +58,75 @@ void SetupWorldMatrix()
 
 #define LOWORD(l) ((WORD)(l))
 
+void PlayHeroesAnimation(ObjectMaster *obj, uint8_t ID, AnimData* animdata, float forcespeed, float forcestate) {
+	EntityData1* data = obj->Data1;
+	float frame = data->Scale.x;
+
+	data->Index = ID;
+	if (forcestate) {
+		frame = forcestate - 1;
+	}
+	else {
+		if (forcespeed) {
+			frame += forcespeed;
+		}
+		else {
+			frame += animdata[ID].AnimationSpeed;
+		}
+	}
+	
+	if (frame == animdata[ID].Animation->motion->nbFrame - 1) {
+		frame = 0;
+
+		if (animdata[ID].Property == 1) {
+			frame = animdata[ID].Animation->motion->nbFrame - 2;
+			if (data->Unknown == 0) data->Unknown = 1;
+		}
+		
+		if (animdata[ID].NextAnim != ID) {
+			data->Index = animdata[ID].NextAnim;
+		}
+		else {
+			if (data->Unknown < 256) {
+				data->Unknown += 1;
+			}
+		}
+	}
+
+	data->Scale.x = frame;
+}
+
 void CreamHeroes_Delete(ObjectMaster *obj) {
-	njReleaseTexture(&CREAM_TEXLIST);
-	obj->Child = nullptr;
+	bool IsThereCream = false;
+
+	for (uint8_t player = 0; player < 8; ++player) {
+		if (player != obj->Data1->CharIndex && PlayerPtrs[player]) {
+			if (Creams[player]) IsThereCream = true;
+		}
+	}
+	
+	if (!IsThereCream) {
+		njReleaseTexture(&CREAM_TEXLIST);
+		CharTexsLoaded[9 - Characters_Cream] = false;
+	}
+
+	Creams[obj->Data1->CharIndex] = nullptr;
 }
 
 void CreamHeroes_Display(ObjectMaster *obj) {
-	if (!obj->Parent) return;
+	ObjectMaster* creamobj = Creams[obj->Data1->CharIndex];
+	if (!creamobj) return;
+	
+	EntityData1* entity1 = obj->Data1;
+	EntityData2* entity2 = (EntityData2*)obj->Data2;
+	CharObj2* co2 = entity2->CharacterData;
+	
+	if (entity1->Action == 13 || entity1->Action == 60) {
+		if (FrameCounterUnpaused % 5 == 0) {
+			if (creamobj->Data1->NextAction == 0) creamobj->Data1->NextAction = 1;
+			else creamobj->Data1->NextAction = 0;
+		}
+	}
 	
 	Direct3D_SetZFunc(1u);
 	Direct3D_PerformLighting(2);
@@ -70,19 +135,17 @@ void CreamHeroes_Display(ObjectMaster *obj) {
 
 	njPushMatrix(0);
 
-	EntityData1* entity1 = obj->Data1;
-	EntityData2* entity2 = (EntityData2*)obj->Data2;
-	CharObj2* co2 = entity2->CharacterData;
-
 	njTranslateV(0, &entity1->Position);
 	njRotateZ(0, entity1->Rotation.z);
 	njRotateX(0, entity1->Rotation.x);
-	njRotateY(0, -LOWORD(entity1->Rotation.y) - 0x4000);
+	njRotateY(0, -entity1->Rotation.y - 0x4000 + creamobj->Data1->Rotation.y);
 
 	SetupWorldMatrix();
 	Direct3D_SetChunkModelRenderState();
 
-	njCnkAction(CreamAnimData[obj->Parent->Data1->Index].Animation, obj->Parent->Data1->Scale.x);
+	if (!creamobj->Data1->NextAction) {
+		njCnkAction(CreamAnimData[creamobj->Data1->Index].Animation, creamobj->Data1->Scale.x);
+	}
 
 	njTranslate(0, 3, 8, 2);
 	DrawChunkObject(CreamMdls[2]->getmodel());
@@ -97,46 +160,274 @@ void CreamHeroes_Display(ObjectMaster *obj) {
 	Direct3D_ResetZFunc();
 }
 
+void CreamHeroes_Main(ObjectMaster *obj) {
+	EntityData1* data = obj->Data1;
+
+	if (data->Action == 0) {
+		obj->DeleteSub = CreamHeroes_Delete;
+		data->CharID = Characters_Cream;
+		data->Action = 1;
+
+		if (!CharTexsLoaded[9 - Characters_Cream]) {
+			CharTexsLoaded[9 - Characters_Cream] = true;
+			LoadPVM("cream", &CREAM_TEXLIST);
+		}
+		
+		return;
+	}
+	else if (data->Action == 1) {
+		data->Action = 2;
+		return;
+	}
+
+	ObjectMaster* playerobj = PlayerPtrs[data->CharIndex];
+	if (!playerobj || playerobj->Data1->CharID != Characters_Tails) DeleteObject_(obj);
+
+	EntityData1* playerdata = playerobj->Data1;
+	EntityData2* playerdata2 = (EntityData2*)playerobj->Data2;
+	CharObj2* playerco2 = playerdata2->CharacterData;
+	
+	switch (data->Action) {
+	case 2:
+		PlayerPtrs[data->CharIndex]->DisplaySub = CreamHeroes_Display;
+
+		int anim = data->Index;
+		float speed = 0;
+		float state = 0;
+		float frame = data->Scale.x;
+
+		if (anim == 19 && playerco2->AnimationThing.Index < 6) {
+			anim = 20; //just stopped rolling
+		}
+		
+		if (anim != 20) {
+			switch (playerco2->AnimationThing.Index) {
+			case 0: anim = 11; break;
+			case 2: anim = 8; break;
+			case 4:
+			case 5:
+			case 6:
+				if (data->Index > 8 && frame == 0.5f && !data->Status) {
+					data->Status = rand() % 2;
+				}
+				anim = 9 + data->Status;
+
+				if (data->Index > 8 && frame > 58) {
+					playerco2->AnimationThing.Index = 0;
+					data->Status = 0;
+				}
+				break;
+			case 9:
+				anim = 0;
+				if (playerco2->Speed.x < 0.02f) anim = 11;
+				break;
+			case 10:
+				anim = 0;
+				speed = 0.9f;
+				break;
+			case 11:
+				anim = 0;
+				speed = 1.5f;
+				break;
+			case 12:
+				anim = 5;
+				break;
+			case 13:
+				anim = 6;
+				speed = 0.5f + playerco2->Speed.x * 0.1f;
+				break;
+			case 14: //jumping
+				if (anim < 18 || anim > 23) {
+					anim = 18;
+				}
+				else if (anim == 19) {
+					if (data->Unknown > 2) {
+						anim = 20;
+					}
+				}
+				else if (anim == 21) {
+					if (playerdata->Position.y - playerco2->_struct_a3.DistanceMax < 10) anim = 22;
+				}
+				break;
+			case 15: //rolling
+			case 16:
+				anim = 19;
+				break;
+			case 17: //spring jump
+				anim = 19;
+				break;
+			case 18: //fall after spring jump
+				anim = 21;
+				break;
+			case 19: //falling
+				anim = 21;
+				if (playerco2->Speed.x > 8 && playerdata->Position.y - playerco2->_struct_a3.DistanceMax > 500) {
+					anim = 16;
+					playerco2->AnimationThing.Index = 150;
+				}
+				break;
+			case 20:
+				anim = 23;
+				break;
+			case 21: //break
+				anim = 47;
+				if (playerco2->Speed.x > 6) {
+					anim = 46;
+				}
+				else if (playerco2->Speed.x > 3) {
+					anim = 45;
+				}
+				break;
+			case 22:
+				anim = 48;
+				break;
+			case 24:
+				anim = 2;
+				speed = 0;
+				break;
+			case 25:
+				anim = 2;
+				break;
+			case 26:
+				anim = 54;
+				break;
+			case 27:
+				anim = 56;
+				break;
+			case 28: //hurt
+			case 29:
+			case 30:
+			case 31:
+				anim = 55;
+				break;
+			case 32:
+				anim = 45;
+				break;
+			case 33: //thumbling
+			case 34:
+				anim = 53;
+				break;
+			case 35:
+				anim = 55;
+				break;
+			case 36:
+				anim = 11;
+				break;
+			case 37: //flying
+			case 40:
+			case 41:
+				anim = 34; //52 IF HOLDING PLAYER
+				if (HeldButtons2[data->CharIndex] & Buttons_A) speed = 0.8;
+				speed += playerco2->Speed.x * 0.5f;
+				break;
+			case 38: //flying falling
+			case 39:
+			case 42:
+				anim = 35;
+				break;
+			case 43:
+				anim = 39;
+				break;
+			case 44:
+			case 45:
+			case 46:
+			case 47:
+			case 48:
+			case 49:
+			case 50:
+			case 51:
+			case 52:
+			case 53:
+				anim = 39;
+				data->Rotation.y += 0x100;
+				break;
+			case 54: //won
+			case 55:
+				anim = 60;
+				if (data->Unknown > 1) {
+					if (frame > 124) data->Unknown = 20;
+					if (frame < 110) data->Unknown = 40;
+
+					if (data->Unknown > 20 && data->Unknown < 40) {
+						++data->Unknown;
+						state = 124 - (data->Unknown - 20);
+					}
+					else if (data->Unknown > 40) {
+						++data->Unknown;
+						state = 110 + (data->Unknown - 40);
+					}
+				}
+				break;
+			case 56: //lost
+			case 57:
+				anim = 10;
+				break;
+			case 58:
+				anim = 44;
+				break;
+			case 72: //holding hook
+				anim = 51;
+				break;
+			case 107: //snowboard
+			case 108:
+			case 109:
+				anim = 26;
+				break;
+			case 110:
+			case 111:
+			case 116:
+				anim = 26;
+				break;
+			case 112:
+				if (playerco2->Speed.z > 0.1f) anim = 33;
+				break;
+			case 113:
+				if (playerco2->Speed.z < 0.1f) anim = 32;
+				break;
+			case 114:
+			case 115:
+			case 117:
+			case 118:
+			case 119:
+				anim = 30;
+				break;
+			case 120:
+			case 121:
+			case 122:
+			case 123:
+			case 124:
+			case 125:
+			case 126:
+				anim = 31;
+				break;
+			case 127:
+				anim = 27;
+				break;
+			case 150:
+				anim = 16;
+				break;
+			}
+		}
+
+		if (anim != 39) {
+			data->Rotation.y = 0;
+		}
+		if (anim != data->Index) {
+			data->Scale.x = 0;
+			data->Unknown = 0;
+		}
+		PlayHeroesAnimation(obj, anim, CreamAnimData, speed, state);
+
+		break;
+	}
+}
+
 void Tails_Display_(ObjectMaster* obj) {
 	if (Creams[obj->Data1->CharIndex]) {
 		CreamHeroes_Display(obj);
 	}
 	else {
 		Tails_Display(obj);
-	}
-}
-
-void CreamHeroes_Main(ObjectMaster *obj) {
-	EntityData1* data = obj->Data1;
-
-	if (data->Action == 0) {
-		obj->DeleteSub = CreamHeroes_Delete;
-		LoadPVM("cream", &CREAM_TEXLIST);
-		data->Action = 1;
-		return;
-	}
-	else if (data->Action == 1) {
-		data->Action = 2;
-
-		PlayerPtrs[data->CharIndex]->Parent = obj;
-		obj->Child = PlayerPtrs[data->CharIndex];
-	}
-
-	if (!obj->Child) DeleteObject_(obj);
-
-	EntityData1* playerdata = obj->Child->Data1;
-	EntityData2* playerdata2 = (EntityData2*)obj->Child->Data2;
-	CharObj2* playerco2 = playerdata2->CharacterData;
-	
-	switch (data->Action) {
-	case 2:
-		PlayerPtrs[data->CharIndex]->DisplaySub = CreamHeroes_Display;
-		EntityData1Ptrs[1]->Position = { -8771, 1303, -2819.688f };
-		EntityData1Ptrs[1]->Rotation = { 0, 0, 0 };
-		data->Index = 1;
-		data->Scale.x += 0.5f;
-
-		break;
 	}
 }
 
@@ -220,6 +511,22 @@ void LoadCreamFiles(const char *path, const HelperFunctions &helperFunctions) {
 		CreamAnimData[i].TransitionSpeed = 0.25f;
 		CreamAnimData[i].AnimationSpeed = 0.5f;
 	}
+
+	CreamAnimData[9].NextAnim = 8;
+	CreamAnimData[10].NextAnim = 8;
+	CreamAnimData[18].NextAnim = 19;
+	CreamAnimData[20].NextAnim = 21;
+	CreamAnimData[22].NextAnim = 23;
+	CreamAnimData[54].Property = 1;
+	CreamAnimData[55].Property = 1;
+	CreamAnimData[60].Property = 1;
+	CreamAnimData[32].Property = 1;
+	CreamAnimData[25].Property = 1;
+	CreamAnimData[30].Property = 1;
+	CreamAnimData[31].Property = 1;
+	CreamAnimData[33].Property = 1;
+	CreamAnimData[56].NextAnim = 11;
+	CreamAnimData[23].AnimationSpeed = 0.25f;
 }
 
 void Characters_Init(const char *path, const HelperFunctions &helperFunctions) {
@@ -237,10 +544,12 @@ void Characters_Init(const char *path, const HelperFunctions &helperFunctions) {
 
 void Characters_OnFrame() {
 	for (uint8_t player = 0; player < 8; ++player) {
-		if (GetCharacterID(player) == Characters_Tails && CreamEnabled) {
-			if (Creams[player] == false) {
-				LoadObject(LoadObj_Data1, 1, CreamHeroes_Main)->Data1->CharIndex = player;
-				Creams[player] = true;
+		if (!EntityData1Ptrs[player]) continue;
+		if (EntityData1Ptrs[player]->CharID == Characters_Tails && CreamEnabled) {
+			if (!Creams[player]) {
+				Creams[player] = LoadObject(LoadObj_Data1, 1, CreamHeroes_Main);
+				Creams[player]->Data1->CharIndex = player;
+				//if (FrameCounterUnpaused > 20) CharObj2Ptrs[player]->AnimationThing.Index = 109;
 			}
 		}
 	}
