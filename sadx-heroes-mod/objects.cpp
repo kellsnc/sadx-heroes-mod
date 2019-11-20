d84cd2e6b7ed95ded243a77adaf3fff5cb53f484
Kell <kellspam@outlook.fr>
2019-11-20 20:04:55 +0100

    Won't use mission mode

--- sadx-heroes-mod/objects.cpp
+++ sadx-heroes-mod/objects.cpp
@@ -9,10 +9,7 @@ NJS_MATERIAL matlist_waterfall[] = {
 };
 
 #define SET_COUNT 4096
-#define MISSION_SET_COUNT 2048
-
 static SETObjData set_table[SET_COUNT]{};
-static MissionSETData mission_set_table[MISSION_SET_COUNT]{};
 
 uint8_t killcount = 0;
 
@@ -64,25 +61,9 @@ void Objects_Init(const char *path, const HelperFunctions &helperFunctions) {
 	WriteData((ObjectMaster***)0x0059222D, &set_table[0].ObjInstance);
 	WriteData((short**)0x0046C1DB, &set_table[SET_COUNT].Flags);
 
-	WriteData((PRM_Entry***)0x591D5F, &mission_set_table[0].PRMEntry);
-	WriteData((MissionSETData**)0x00591D64, &mission_set_table[0]);
-	WriteData((MissionSETData**)0x00592245, &mission_set_table[0]);
-	WriteData((float**)0x0059224C, &mission_set_table[MISSION_SET_COUNT - 1].Distance);
-	WriteData((MissionSETData**)0x00592268, &mission_set_table[0]);
-	WriteData((float**)0x0059226F, &mission_set_table[MISSION_SET_COUNT - 1].Distance);
-	WriteData((MissionSETData**)0x005922AA, &mission_set_table[0]);
-	WriteData((float**)0x005922B5, &mission_set_table[MISSION_SET_COUNT - 1].Distance);
-	WriteData((char**)0x5922FB, &mission_set_table[0].LoadCount);
-	WriteData((short**)0x00591BAF, &mission_set_table[0].Flags);
-	WriteData((ObjectMaster***)0x00591A2D, &mission_set_table[0].ObjInstance);
-	WriteData((ObjectMaster***)0x00591C26, &mission_set_table[0].ObjInstance);
-	WriteData((ObjectMaster***)0x0059221D, &mission_set_table[0].ObjInstance);
-
 	WriteData((int*)0x0046BD64, SET_COUNT);
 	WriteData((int*)0x0046BD6C, SET_COUNT);
 	WriteData((int*)0x0046BF3D, SET_COUNT);
 	WriteData((int*)0x0046BF44, SET_COUNT);
 	WriteData((int*)0x0046C1E0, (SET_COUNT + 1));
-
-	WriteData((int*)0x00591A32, MISSION_SET_COUNT);
 }
